9ce059405bed24ae946ff5c8dffdb66e1fcbb2d5
Robert Saramet <rob3rt.saramet@gmail.com>
2020-06-20 17:05:06 +0300

    Documentation

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,20 @@
     -----------------------------------------------------------
 
 
-    DHT11 VCC => Shield 5V
-    DHT11 GND => Shield GND
+    ---------------------------------
+    |   DHT11 VCC   |   Shield 5V   |
+    |   DHT11 GND   |   Shield GND  |
+    |   DHT11 OUT   |   Shield D2   |
+    ---------------------------------
+    |   Rain VCC    |   Mega D22    |
+    |   Rain GND    |   Shield GND  |
+    |   Rain OUT    |   Shield A0   |
+    ---------------------------------
+    |   LCD VCC     |   Mega D24    |
+    |   LCD GND     |   Shield GND  |
+    |   LCD SDA     |   Mega SDA    |
+    |   LCD SCL     |   Mega SCL    |
+    ---------------------------------
 */
 
 #include <Arduino.h>
