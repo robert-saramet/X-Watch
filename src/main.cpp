370713f572b3621fe968de22abb547a96f72efa1
Robert Saramet <rob3rt.saramet@gmail.com>
2020-06-20 15:22:24 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+byte mac[] {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 0, 190);
+EthernetServer server(80);
+
+const int rainPin=A0;
+const int treshold=500;
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
